6c91ea99b4743e11686e6248e4bc0b89e3f37c45
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-15 06:27:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ status = get_price()
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"CREO {status}% | LucyHarun"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${status}"))
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    msg = message.content 
-    if msg.startswith("!refresh"):
+    msg = message.content
         refresh = get_price()
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"CREO {refresh}% | LucyHarun"))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${refresh}"))
 client.run(os.getenv("TOKEN"))
