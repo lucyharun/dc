e3a9afb3c7c02878fde23579638fd456b7844437
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-15 03:12:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import requests
 import json
 
 client = discord.Client()
-def get_price():
+def get_pricechange():
     response = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=creo-engine")
     json_data = json.loads(response.text)
-    price = json_data[0]["current_price"]
     pricechange = json_data[0]["price_change_24h"]
-    return (pricechange)(price)
+    return (pricechange)
  status = pricechange()
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${pricechange}"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${status}"))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     msg = message.content
-    if msg.startswith("!price"):
-        price = get_price()
-        await message.channel.send(f"**CREO Price**: ${price}")  
+    if msg.startswith("!pricechange"):
+        pricechange = get_pricechange()
+        await message.channel.send(f"**CREO Price Change 24h**: ${pricechange}")  
     if msg.startswith("!refresh"):
-        refresh = get_price()
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${status}"))
+        refresh = get_pricechange()
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${refresh}"))
 client.run(os.getenv("TOKEN"))
