47d232367c240964ee821b079f62101f44edce6d
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-15 06:46:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ async def on_message(message):
     if message.author == client.user:
         return
     msg = message.content
+    if msg.startswith("!price"):
+        price = get_price()
+        await message.channel.send(f"**Bitcoin Price**: ${price}")  
+    if msg.startswith("!refresh"):
         refresh = get_price()
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${refresh}"))
 client.run(os.getenv("TOKEN"))
