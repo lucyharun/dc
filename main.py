67f24aac0a1abcad28aad66611ea86f68529db1f
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-15 02:42:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ client = discord.Client()
 def get_price():
     response = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=creo-engine")
     json_data = json.loads(response.text)
-    price = json_data[0]["price_change_24h"]
+    price = json_data[0]["current_price"]
     return (price)
-status = get_price()       
+    pricechange = json_data[0]["price_change_24h"]
+    return (pricechange)
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${status}"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${pricechange}"))
 
 @client.event
 async def on_message(message):
@@ -25,5 +26,5 @@ async def on_message(message):
         await message.channel.send(f"**CREO Price**: ${price}")  
     if msg.startswith("!refresh"):
         refresh = get_price()
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"CREO"))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${pricechange}"))
 client.run(os.getenv("TOKEN"))
