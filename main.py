368a842079530770a2be6d00bca40fcf95dac67b
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-15 16:31:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ client = discord.Client()
 def get_price():
     response = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=creo-engine")
     json_data = json.loads(response.text)
-    price = json_data[0]["price_change_percentage_24h"]
+    price = json_data[0]["rice_change_percentage_24h"]
     return (price)
 status = get_price()       
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord! ')
-    for guild in client.guilds:
-        print("connected to ", guild.name)
-    refresh_price.start()
+    print("We have logged in as {0.user}".format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"CREO {status}% | LucyHarun"))
 
-@tasks.loop(seconds=float(REFRESH_TIMER))
-async def refresh_price():
-    for guild in client.guilds:
-        await guild.me.edit(activity=discord.Activity(type=discord.ActivityType.watching, name=f"CREO {status}% | LucyHarun"))
-@tasks.loop(seconds=float(REFRESH_TIMER))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content
+    if msg.startswith("!refresh"):
+        refresh = get_price()
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"${refresh}"))
 client.run(os.getenv("TOKEN"))
